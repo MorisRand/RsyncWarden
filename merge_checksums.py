6da693d84705140d1630b4a6480174d79b33df19
Konstantin Treskov <treskov@jinr.ru>
2020-07-21 11:01:12 +0300

    Output checksums for goodrun list

--- merge_checksums.py
+++ merge_checksums.py
@@ -8,23 +8,33 @@ import argparse
 
 
 def merge_checksums(args):
-    checksums = set()
+    checksums_pathes = set()
+    checksums_full = set()
     for  csum_file in glob.glob(args.checksums+'/checksums*'):
         with open(csum_file, 'r') as f:
             for line in f:
                 cksum, _, path = line.split(' ')
-                checksums.add(path)
+                checksums_pathes.add(path)
+                checksums_full.add((path, cksum))
 
     goodruns = set()
-    with open(args.good_runs) as f:
+    with open(args.good_runs, 'r') as f:
         for line in f:
             goodruns.add(line)
 
-    missing_pathes = goodruns.difference(checksums)
+    missing_pathes = goodruns.difference(checksums_pathes)
     print('Found {} missing files without checksums'.format(len(missing_pathes)))
     with open(args.missing_files, 'w') as f:
         for path in missing_pathes:
             f.write(path)
+
+    if args.goodrun_checksums:
+        with open(args.goodrun_checksums, 'w') as f:
+            for path, cksum in sorted(checksums_full):
+                if path in goodruns:
+                    cksum = cksum.rstrip('\n')
+                    path = path.rstrip('\n')
+                    f.write(f'{path} {cksum}\n')
 
 
 if __name__ == "__main__":
@@ -35,6 +45,8 @@ if __name__ == "__main__":
             help='Path to good run list')
     parser.add_argument('--missing-files', type=abspath, required=True,
             help='Path where missing files list will be created')
+    parser.add_argument('--goodrun-checksums', type=abspath,
+            help='Path to file where checksums and pathes for goodrun list will be stored')
     
     args = parser.parse_args()
     merge_checksums(args)
