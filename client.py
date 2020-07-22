2d070e17e8b66e1343a37fa5f816623c90d41259
Konstantin Treskov <treskov@jinr.ru>
2020-07-22 10:59:01 +0300

    start rsync

--- client.py
+++ client.py
@@ -15,14 +15,14 @@ from common.models import ClientMessage, Status
 
 RSYNC_VERBOSE_COMMAND = '''rsync -h --progress -avzp --ignore-existing
                            --files-from={filelist}  rsync://{ihep_host}:/dybfs {eos_home}'''
-RSYNC_REWRITE_COMMAND = '''rsync -h --progress -avzp 
+RSYNC_SILENT_COMMAND = '''rsync -avzp --ignore-existing
                             --files-from={filelist}  rsync://{ihep_host}:/dybfs {eos_home}'''
-RSYNC_SILENT_COMMAND = '''rsync -avzp 
+RSYNC_REWRITE_COMMAND = '''rsync -h --progress -avzp 
                             --files-from={filelist}  rsync://{ihep_host}:/dybfs {eos_home}'''
 
 def get_config():
     try:
-        with open(abspath("client_config.yaml"), 'r') as f:
+        with open(abspath("client_conf.yaml"), 'r') as f:
             config = yaml.load(f)
     except FileNotFoundError:
         logger.info('Failed to find config, trying environmetal vars')
@@ -33,6 +33,11 @@ def get_config():
         config['eos_home'] = os.environ['EOS_HOME']
         config['ihep_host'] = os.environ['IHEP_HOST']
     return config
+
+def compute_md5(path):
+    s = subprocess.check_output(f"md5sum {path}").decode()
+    # subporcess returns "cksum path", need to extract cksum
+    return s.split()[0]
 
 
 @logger.catch()
@@ -67,13 +72,14 @@ def event_loop(config):
     ihep_host = config['ihep_host']
     eos_home = config['eos_home']
 
-    copying_in_progress, checking_cksums = False
+    copying_in_progress, checking_cksums = False, False
+    temp, rsync_process = None, None
     while True:
         if not copying_in_progress:
             run, pathes, cksums = get_new_run(server, credentials)
-
-        logger.info(f'Starting new copy process for {run}')
-        with NamedTemporaryFile(mode='w+t') as temp:
+            temp =  NamedTemporaryFile(mode='w+t')
+            logger.debug(f'Created temporaty file {temp.name} to fill with pathes to transfer in {run}')
+            
             # prepare the file list for rsync process
             for path in pathes:
                 temp.write(path+'\n')
@@ -81,6 +87,22 @@ def event_loop(config):
             temp.seek(0)
 
             filelist = temp.name
+            rsync_command = RSYNC_SILENT_COMMAND.format(**locals())
+            logger.debug(f'Executing {rsync_command}')
+
+            logger.info(f'Starting new copy process for {run}')
+            rsync_process = subprocess.Popen(rsync_command.split())
+
+            copying_in_progress = True
+        else:
+            # when rsync terminate, close temporary file
+            if rsync_process.poll():
+                temp.close()
+                
+                
+                # compute and compare checksums:
+
+                # report results to warden server
 
             time.sleep(5)
 
@@ -88,11 +110,9 @@ def event_loop(config):
 
 
 
-
-
 if __name__ == "__main__":
-    logger.add('client_{}.log'.format(os.getpid()), backtrace=True,
-            diagnose=True, rotation='500 MB')
+    logger.add('warden_client_{}.log'.format(os.getpid()), backtrace=True,
+                diagnose=True, rotation='500 MB')
     config = get_config()
     event_loop(config)
     
